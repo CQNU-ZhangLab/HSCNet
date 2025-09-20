a78703e13f44a8f432b933bf56cea3188dc4d923
CQNU-ZhangLab <163380281+CQNU-ZhangLab@users.noreply.github.com>
2025-09-20 23:10:22 +0800

    Delete val.py

--- val.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.filterwarnings('ignore')
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    model = YOLO('best.pt')
-    result = model.val(data='ultralytics/cfg/datasets/VisDrone.yaml', imgsz=1024, split='val', project='runs/val', name='exp')
